b3e14a85e0c6aa4a53a70fb99a72c8cfccd28092
alex <palaciosalex07@gmail.com>
2026-06-19 14:07:56 -0500

    crea componente lista de tareas

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="sidebar">
+<h2>Barra lateral</h2>
+<ul>
+    <li><router-link to="/contador">Contador</router-link></li>
+    <li><router-link to="/lista-de-tareas">Lista de Tareas</router-link></li>
+    
+</ul>
+
+</div>
+
+</template>
+
+<script setup>
+import router from '@/router';
+
+
+
+</script>
+
+
+<style>
+.sidebar{
+    width: 200px;
+    background-color: #f0f0f0;
+    padding: 20px;
+}
+</style>
